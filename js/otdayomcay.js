7a06c4379152f7d3efdb41f97dbdf106392897a0
Zachary Velcoff <zackfactual@gmail.com>
2018-07-27 21:46:03 -0700

    base case translation

--- js/otdayomcay.js
+++ js/otdayomcay.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
 });
 
 function translateText(){
-    // initialize letter arrays — don't do this in the function unless you have to
-    var vowels = ["a","e","i","o","u"];
-    var consonants = ["b","c","d","f","g","h","i","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+    // initialize letter arrays — make these global ASAP
+    var vowels = ["a","e","i","o","u","A","E","I","O","U"];
+    var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"];
+    
+    // clear translation div
+    document.getElementById("translation").innerHTML = "";
     
     // assign input to a variable
     var textblock = document.getElementById("textToTranslate").value;
@@ -20,20 +23,21 @@ function translateText(){
     // iterate over words
     for (i = 0; i < textblock.length; i++){
         var word = textblock[i].split("");
-        var newword = "-"; 
+        var afterHyphen = "-"; 
         
         for (j = 0; j < word.length; j++){
             if (consonants.includes(word[j])){
-                newword += word[j];
+                afterHyphen += word[j];
             }
             else if (vowels.includes(word[j])){
-                var front = word.slice(word[j],word.length + 1);
+                var beforeHyphen = word.slice(j,word.length + 1);
                 break;      
             }
         }
-        newword += "ay";
-        front += newword;
-        document.getElementById("translation").append(front);
+        afterHyphen += "ay";
+        beforeHyphen = beforeHyphen.join("");
+        var latinWord = beforeHyphen + afterHyphen;
+        document.getElementById("translation").append(latinWord + " ");
     }
 }
 
