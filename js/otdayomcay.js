10a33cc28f8f1baded501a6fdffc66dc97bb3a5f
Zachary Velcoff <zackfactual@gmail.com>
2018-08-21 17:47:33 -0700

    work for words starting with vowels or consonants, still have to fix punctuation

--- js/otdayomcay.js
+++ js/otdayomcay.js
@@ -1,43 +1,49 @@
-$(document).ready(function(){ 
-    // auto-update copyright
-    var year = (new Date).getFullYear();
-    $("#copy").append("&copy; " + year + " — Zachary Velcoff");
-    
-    
-});
+// initialize letter arrays
+var VOWELS = ["a","e","i","o","u","A","E","I","O","U"]
+var CONSONANTS = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"];
 
-function translateText(){
-    // initialize letter arrays — make these global ASAP
-    var vowels = ["a","e","i","o","u","A","E","I","O","U"];
-    var consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"];
-    
+function translateText(){    
     // clear translation div
     document.getElementById("translation").innerHTML = "";
     
     // assign input to a variable
-    var textblock = document.getElementById("textToTranslate").value;
+    var textBlock = document.getElementById("textToTranslate").value;
     
     // split input into an array of words
-    textblock = textblock.split(" ");
+    textBlock = textBlock.split(" ");
     
     // iterate over words
-    for (i = 0; i < textblock.length; i++){
-        var word = textblock[i].split("");
-        var afterHyphen = "-"; 
+    for (i = 0; i < textBlock.length; i++) { 
+        var latinWord = "";
+        // if word starts with a vowel
+        if(VOWELS.includes(textBlock[i].charAt(0))) {
+            // append "ay" (way? yay?) to it and move on with your life
+            latinWord = textBlock[i] + "ay";
+        }
+        // if word starts with a consonant or consonant cluster
+        else {
+            // split the word into a character array
+            var wordArray = textBlock[i].split("");
+            var firstHalf = "";
+            var secondHalf = "";
         
-        for (j = 0; j < word.length; j++){
-            if (consonants.includes(word[j])){
-                afterHyphen += word[j];
-            }
-            else if (vowels.includes(word[j])){
-                var beforeHyphen = word.slice(j,word.length + 1);
-                break;      
+            for (j = 0; j < wordArray.length; j++) {
+                // build up consonant cluster
+                if (CONSONANTS.includes(wordArray[j])) {
+                    secondHalf += wordArray[j];
+                }
+                // assign firstHalf the joined wordArray from its current index to its end
+                else {
+                    firstHalf = wordArray.slice(j, wordArray.length).join("");
+                    secondHalf += "ay";
+                    latinWord = firstHalf + secondHalf;
+                    break;
+                }
             }
         }
-        afterHyphen += "ay";
-        beforeHyphen = beforeHyphen.join("");
-        var latinWord = beforeHyphen + afterHyphen;
+        // DEAL WITH PUNCTUATION IN AND AT END OF WORDS
+        
+        // display results
         document.getElementById("translation").append(latinWord + " ");
     }
 }
-
