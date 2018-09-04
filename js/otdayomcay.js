48442f10e40f2ba61b4ac1531993549516933926
Zachary Velcoff <zackfactual@gmail.com>
2018-09-04 15:06:14 -0700

    remove unused scripts, switch to unobtrusive

--- js/otdayomcay.js
+++ js/otdayomcay.js
@@ -2,6 +2,10 @@
 var VOWELS = ["a","e","i","o","u","A","E","I","O","U"]
 var CONSONANTS = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"];
 
+window.onload = function() {
+    document.getElementById("translateButton").addEventListener("click", translateText);
+};
+
 function translateText() {    
     // clear translation div
     document.getElementById("translationDiv").style.display = "block";
